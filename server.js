2ab36c23aee8741136a2f9158816899037a1b700
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-05-07 13:06:15 -0400

    adding a for loop instead of the filter function; Bug solved

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const app= express();
+const app = express();
 const { v4: uuidv4 } = require('uuid');
 
 //creates the PORT OR use the localhost:3000
@@ -9,22 +9,22 @@ const PORT = process.env.PORT || 3000;
 
 //body parsing, static and route middleware 
 //express.urlendoced and express.json are used for POST and PUT requests
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //it adds the static middleware and starts listening on a port, __dirname is the root directory in this case
 app.use(express.static('public'));
 
 //app.get() functions
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,"./public/index.html"))
+    res.sendFile(path.join(__dirname, "./public/index.html"))
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname,"./public/notes.html"))
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
 });
 
 app.get('/api/notes', (req, res) => {
-    fs.readFile(path.join(__dirname,"./db/db.json"), (err, data) => {
+    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
         if (err) throw err;
         return res.json(JSON.parse(data));
     });
@@ -32,14 +32,14 @@ app.get('/api/notes', (req, res) => {
 
 //app.post() function
 app.post('/api/notes', (req, res) => {
-    fs.readFile(path.join(__dirname,"./db/db.json"), (err, data)=>{
+    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
         if (err) throw err;
         let savedData = JSON.parse(data);
         savedData.push(req.body);
         //req.body.id=req._server.Symbol(async_id_symbol);
         //include a universal unique id
         req.body.id = uuidv4();
-        console.log('id of the request: '+ req.body.id);
+        console.log('id of the request: ' + req.body.id);
 
         res.json(fs.writeFile('./db/db.json', JSON.stringify(savedData), err => {
             if (err) throw err;
@@ -49,23 +49,40 @@ app.post('/api/notes', (req, res) => {
 });
 
 //app.delete function
+//Ask TA: how to connect the id of the notes
 app.delete('/api/notes/:id', (req, res) => {
-    fs.readFile(path.join(__dirname, './db/db.json'),(err, data) => {
-        if(err) throw err;
+    fs.readFile(path.join(__dirname, './db/db.json'), (err, data) => {
+        if (err) throw err;
         console.log(req.params.id);
         savedData = JSON.parse(data);
+        console.log(savedData);
+        const filteredArr = []; 
         //creates a new array filled with elements do not have the selected id.
-        savedData = savedData.filter(({id})=>{id!==req.params.id});
-    
+      // const newArr = savedData.filter(notes => { notes.id !== req.params.id });
+        for (let i = 0; i < savedData.length; i++) {
+            const element = savedData[i];
+            //console.log(element); 
+            if(element.id !== req.params.id) {
+                filteredArr.push(element);
+            }
+        }
+        console.log(filteredArr);
         //overwrite the json file with the newly created array
-    res.json(fs.writeFile('./db/db.json', JSON.stringify(savedData), err=>{
-        if (err) throw err;
-        console.log("You have deleted this note.")
-        }));
+        fs.writeFile('./db/db.json', JSON.stringify(filteredArr), err=>{
+            if (err) throw err;
+            //console.log("You have deleted this note.")
+            res.send(`note ${req.params.id} has been deleted`);
+        });
     });
 });
 
+
+app.get('*', (req, res) => {
+    //it would be cool to make a customized 404 html page.
+    res.send('sorry 404');
+});
+
 //Display the message once port has established.
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log('listening on PORT: ' + PORT);
 })
